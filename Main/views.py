b2d34cdde2786320fad3a8b75511c06c34777748
Abraham Francis <abfr049@gmail.com>
2019-10-06 00:44:36 +0530

    add index router

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index_view(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    return HttpResponse(405)
