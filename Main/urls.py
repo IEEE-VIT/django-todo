ce4821e6e87a9e9b4bfcfb66f33af1d27c118d2c
Craig McAulay <craigmcaulay98@googlemail.com>
2019-10-06 17:16:43 +0100

    Added signup process & success redirect

--- Main/urls.py
+++ Main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, signup_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('signup/', signup_view, name='signup'),
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,3 +9,25 @@ def index_view(request):
     if request.method == 'GET':
         return render(request, 'Index.html')
     return HttpResponse(405)
+
+def signup_view(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+        return render(request, '../templates/Main/signup.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():      
+            form.save()
+    
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+
+            login(request, user)
+            return render(request, '../templates/Main/success.html')
+        else:
+            return HttpResponse(405)
+    else:
+        return HttpResponse(405)
