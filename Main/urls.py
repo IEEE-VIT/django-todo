7787e695d219da16f7449c5c5029b85a05daa1f2
Manish <itzmanish108@gmail.com>
2019-10-06 05:39:22 +0000

    Added: Signup view with respective url, Signup and success page.

--- Main/urls.py
+++ Main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, signup_view, success_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('success/', success_view, name='success-signup'),
+    path('signup/', signup_view, name='signup'),
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def index_view(request):
     if request.method == 'GET':
-        return render(request, 'Index.html')
+        return render(request, 'Main/index.html')
+    return HttpResponse(405)
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(request, user)
+        return redirect("success-signup")
+    else:
+        form = UserCreationForm()
+    return render(request, 'Main/signup.html', {'form':form})
+
+def success_view(request):
+    if request.method == 'GET':
+        return render(request, 'Main/success-signup.html')
     return HttpResponse(405)
