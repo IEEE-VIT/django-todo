02977fd6b6d494f822fd478485af1395dcf6fd7b
Rahul Palve <hi.rahulpalve@gmail.com>
2019-10-06 11:23:21 +0530

    1. Added 'Main' as installed app in settings
    2. Created 'Index.html' template
    3. Created 'login' and 'signUp' views
    4. Added urls for 'login' and 'signUp' views

--- Main/urls.py
+++ Main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, login_view, signUp_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('login/', login_view, name='login'),
+    path('signup/', signUp_view, name='signUp'),
+
 ]
--- Main/views.py
+++ Main/views.py
@@ -7,3 +7,10 @@ def index_view(request):
     if request.method == 'GET':
         return render(request, 'Index.html')
     return HttpResponse(405)
+
+def signUp_view(request):
+    return HttpResponse(405)
+    
+
+def login_view(request):
+    return HttpResponse(405)
