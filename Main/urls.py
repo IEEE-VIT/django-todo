800cd44c51fb96d8f2af682c732c636a3b6f2b3d
Manish <12438068+itzmanish@users.noreply.github.com>
2019-10-06 13:18:01 +0530

    Update urls.py as suggested

--- Main/urls.py
+++ Main/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import index_view, signup_view, success_view
+from .views import index_view, signup_view
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('success/', success_view, name='success-signup'),
     path('signup/', signup_view, name='signup'),
 ]
