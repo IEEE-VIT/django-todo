4b78c737c650279ea82c8c727c5ad2cfb73879ff
aryan <aryan@Aryans-MacBook-Pro.local>
2019-10-15 23:23:43 +0530

    Modified tasks

--- Main/models.py
+++ Main/models.py
@@ -5,8 +5,9 @@ class Tasks(models.Model):
 	title = models.CharField(max_length=50)
 	description = models.TextField()
 	created = models.DateTimeField(auto_now_add=True)
-	deadline = models.DateTimeField()
+	deadline = models.DateTimeField(required=False)
 	done = models.BooleanField(default=False)
+	
 
 	def __str__(self):
 		return self.title
